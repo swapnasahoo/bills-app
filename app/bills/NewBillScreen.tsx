8e9d65f32cca2325af85cb6ed6f606d6782bd0ba
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-11-24 17:06:32 +0530

    Added dropdown for selecting month

--- app/bills/NewBillScreen.tsx
+++ app/bills/NewBillScreen.tsx
@@ -1,6 +1,7 @@
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { router } from "expo-router";
 import { Pressable, ScrollView, Text, TextInput, View } from "react-native";
+import RNPickerSelect from "react-native-picker-select";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const NewBillScreen = () => {
@@ -27,8 +28,38 @@ const NewBillScreen = () => {
         >
           {/* INPUT FIELDS */}
           <View className="mt-4 w-full gap-4">
+            <Text className="text-white text-lg">Month</Text>
+            <RNPickerSelect
+              onValueChange={(value) => console.log(value)}
+              items={[
+                { label: "January", value: "January" },
+                { label: "February", value: "Feburary" },
+                { label: "March", value: "March" },
+                { label: "April", value: "April" },
+                { label: "May", value: "May" },
+                { label: "June", value: "June" },
+                { label: "July", value: "July" },
+                { label: "August", value: "August" },
+                { label: "September", value: "September" },
+                { label: "October", value: "October" },
+                { label: "November", value: "November" },
+                { label: "December", value: "December" },
+              ]}
+              style={{
+                inputAndroid: {
+                  color: "white",
+                  backgroundColor: "#374151",
+                },
+                inputIOS: {
+                  color: "white",
+                  backgroundColor: "#374151",
+                },
+                placeholder: {
+                  color: "#9ca3af",
+                },
+              }}
+            />
             {[
-              { label: "Month", placeholder: "January" },
               { label: "Date", placeholder: "12/11/2025" },
               { label: "Rent", placeholder: "0" },
               { label: "Fix", placeholder: "0" },
