e889ee507f3abd96eb3a2226511bf6d64e5f022c
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-11-24 10:36:55 +0530

    Added context functions for adding and deleting bills

--- context/UserDataContext.tsx
+++ context/UserDataContext.tsx
@@ -7,6 +7,7 @@ interface User {
 }
 
 interface BillEntry {
+  id: number;
   date: number;
   rent: number;
   fix: number;
@@ -24,6 +25,8 @@ interface UserDataContextType {
   addUser: (id: number, name: string, roomNo: number) => void;
   deleteUser: (id: number) => void;
   updateUser: (id: number, name: string, roomNo: number) => void;
+  addBill: (id: number, entry: BillEntry) => void;
+  deleteBill: (id: number) => void;
 }
 
 export const UserDataContext = createContext<UserDataContextType>({
@@ -32,11 +35,15 @@ export const UserDataContext = createContext<UserDataContextType>({
   addUser: () => {},
   deleteUser: () => {},
   updateUser: () => {},
+  addBill: () => {},
+  deleteBill: () => {},
 });
 
 function UserDataContextProvider({ children }: { children: React.ReactNode }) {
   const [users, setUsers] = useState<User[]>([]);
-  const [userBills, setUserBills] = useState({});
+  const [userBills, setUserBills] = useState<{ [key: number]: BillEntry[] }>(
+    {}
+  );
 
   function addUser(id: number, name: string, roomNo: number) {
     setUsers((prev) => [...prev, { id: id, name: name, roomNo: roomNo }]);
@@ -54,9 +61,32 @@ function UserDataContextProvider({ children }: { children: React.ReactNode }) {
     );
   }
 
+  function addBill(id: number, entry: BillEntry) {
+    setUserBills((prev) => ({
+      ...prev,
+      [id]: prev[id] ? [...prev[id], entry] : [entry],
+    }));
+  }
+
+  function deleteBill(id: number) {
+    setUserBills((prev) => {
+      const updated = { ...prev };
+      delete updated[id];
+      return updated;
+    });
+  }
+
   return (
     <UserDataContext.Provider
-      value={{ users, userBills, addUser, deleteUser, updateUser }}
+      value={{
+        users,
+        userBills,
+        addUser,
+        deleteUser,
+        updateUser,
+        addBill,
+        deleteBill,
+      }}
     >
       {children}
     </UserDataContext.Provider>
